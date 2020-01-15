a0fb162d95f80e3f558f58a8fe34298e08485338
Dimitrije Mitic <dimitrije.mitic@oasisdigital.com>
2020-01-15 18:11:20 -0500

    updates to deploy to heroku 2

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/personal-website'));
+app.use(express.static(__dirname + '/dist/mitidimi'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/personal-website/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/mitidimi/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
